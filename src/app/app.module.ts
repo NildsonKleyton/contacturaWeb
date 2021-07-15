c7f4e1dc4d29c2b9431c7392317a3a7e730d3514
Nildson <nildsonkleyton@hotmail.com>
2021-07-15 20:00:08 -0300

    Contactura aula4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { ListaContatosComponent } from './lista-contatos/lista-contatos.component';
+import { ListaUsuariosComponent } from './lista-usuarios/lista-usuarios.component';
+import { FormUsuariosComponent } from './form-usuarios/form-usuarios.component';
+import { FormContatosComponent } from './form-contatos/form-contatos.component';
+import { NotFoundComponent } from './sharedComponents/not-found/not-found.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+//importes do material debugging
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ListaContatosComponent,
+    ListaUsuariosComponent,
+    FormUsuariosComponent,
+    FormContatosComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
